425c36b4ff57f02076181b6378d67ddf2abe70fb
Justin Studer <justin.studer@bluewin.ch>
2022-12-14 16:43:25 +0100

    Added ConstantsScript

---
+++ Assets/ConstantsScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantsScript : MonoBehaviour
+{
+
+    public float gravity = 9.81f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
