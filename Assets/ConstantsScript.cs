55b315e2cff5fd1e65539a8f1e61bf04711e27dd
Justin Studer <justin.studer@bluewin.ch>
2022-12-16 15:22:56 +0100

    Changed sliders to touch and added debug slider

--- Assets/ConstantsScript.cs
+++ Assets/ConstantsScript.cs
@@ -8,4 +8,6 @@ public class ConstantsScript : MonoBehaviour
     public float Gravity = 9.81f;
     public float DartsSpeed = 1000;
 
+    public GameObject InputController;
+
 }
--- Assets/Scripts/GestureUtils.cs
+++ Assets/Scripts/GestureUtils.cs
@@ -1,18 +1,46 @@
 using Microsoft.MixedReality.Toolkit.Utilities;
-
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public static class GestureUtils
 {
-    private const float PinchThreshold = 0.7f;
+    private const float PinchThresholdPlacement = 0.7f;
+    private const float PinchThresholdThrowing = 0.2f;
     private const float GrabThreshold = 0.4f;
 
     public static bool IsPinching(Handedness trackedHand)
     {
-        return HandPoseUtils.CalculateIndexPinch(trackedHand) > PinchThreshold;
+        List<GameObject> rootObjects = new List<GameObject>();
+        Scene scene = SceneManager.GetActiveScene();
+
+        scene.GetRootGameObjects(rootObjects);
+
+        GameObject InputController = null;
+
+        foreach (GameObject go in rootObjects)
+        {
+            if (go.name == "Constants")
+            {
+                InputController = go.GetComponent<ConstantsScript>().InputController;
+            }
+        }
+
+        float currentThreshold = PinchThresholdPlacement;
+        if (InputController.GetComponent<InputController>().mode == Modes.Dart || InputController.GetComponent<InputController>().mode == Modes.Idle)
+        {
+            currentThreshold = PinchThresholdThrowing;
+        }
+
+        //Debug.Log($"{currentThreshold}");
+
+
+        return HandPoseUtils.CalculateIndexPinch(trackedHand) > currentThreshold;
     }
 
     public static bool IsGrabbing(Handedness trackedHand)
     {
+
         return !IsPinching(trackedHand) &&
                HandPoseUtils.MiddleFingerCurl(trackedHand) > GrabThreshold &&
                HandPoseUtils.RingFingerCurl(trackedHand) > GrabThreshold &&
